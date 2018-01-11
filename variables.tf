9ae833c95ed6481e4675180acf1afaa907cefb5c
Cocomico <Cocomico@web.de>
2018-01-11 14:28:57 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -178,24 +178,10 @@ variable "disk_size"
   Following JSON is the variable annotation JSON
   {
     "visible" : true,
-    "options" : [
-      {
-        "value": "250",
-        "label": "250 GB"
-      }, 
-      {
-        "value": "251",
-        "label": "250+1 GB"
-      }, 
-      {
-        "value": "252",
-        "label": "250+2 GB"
-      }, 
-      {
-        "value": "500",
-        "label": "500 GB"
-      }
-    ]
+    "options" : [ { "value" : 250, "label" : "250 GB" }, 
+                   { "value" : 251, "label" : "250+1 GB" }, 
+                   { "value" : 252, "label" : "250+2 GB" }, 
+                   { "value" : 500, "label" : "500 GB" } ]
   }
 */
 {
