aee060e590918ed7a813171bc200757e7a8ab048
Cocomico <Cocomico@web.de>
2018-01-11 14:25:10 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -173,23 +173,27 @@ variable "optionsTestRequired"
   type = "string"
 }
 
-variable "disksize"
+variable "disk_size"
 /*
   Following JSON is the variable annotation JSON
   {
     "visible" : true,
     "options" : [
       {
-        "value": "250", "label": "250 GB"
+        "value": "250",
+        "label": "250 GB"
       }, 
       {
-        "value": "251", "label": "250+1 GB"
+        "value": "251",
+        "label": "250+1 GB"
       }, 
       {
-        "value": "252", "label": "250+2 GB"
+        "value": "252",
+        "label": "250+2 GB"
       }, 
       {
-        "value": "500", "label": "500 GB"
+        "value": "500",
+        "label": "500 GB"
       }
     ]
   }
